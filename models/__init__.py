a36d3cfe14e2b62de732b088fa0433ce8df6d01c
antrip03 <anshultripathi002@gmail.com>
2026-01-05 18:22:04 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,7 @@
 # models/__init__.py
 
-from .encoder import CNNBiLSTMEncoder
-from .decoder import GapDecoder
 from .transformer_encoder import DNAMaskedEncoder  # NEW
 
 __all__ = [
-    "CNNBiLSTMEncoder",
-    "GapDecoder",
     "DNAMaskedEncoder",   # NEW
 ]
